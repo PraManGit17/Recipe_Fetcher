402eb51637935dbd9c94ccedec803c1e78e010f6
Your NamePratham <pra172005@gmail.com>
2025-03-04 06:40:51 +0530

    landing commit

--- Frontend/front_work/src/components/Header.jsx
+++ Frontend/front_work/src/components/Header.jsx
@@ -6,36 +6,39 @@ import { motion } from "framer-motion";
 const Header = () => {
   return (
     <motion.header
-      initial={{ opacity: 0 , y: "-100%"}}
-      animate={{ opacity: 1 , y: 0}}
-      transition={{ duration: 0.6, ease: "easeOut"  }}
-      className="flex justify-between items-center overflow-hidden p-2  shadow-lg mx-5 mt-5 border-white border-2 rounded-2xl"
+      initial={{ opacity: 0, y: "-100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      className="flex justify-between items-center overflow-hidden p-1 mx-5 mt-5 border-white border-2 rounded-2xl"
       style={{
-        background: 'linear-gradient(90deg, #2D2D2D, #535353, #5B5B5B)',
+        background: 'linear-gradient(90deg, #1C1C1C, #2B2B2B, #3A2F2F)',
+        background: 'rgba(28, 28, 28, 0.85)',
+        backdrop_filter: 'blur(10px)',
+
       }}
     >
-      {/* Left Section */}
-      <div className="flex items-center space-x-3">
-        <img src={logo} alt="Logo" className="h-8 w-8" />
+
+
+      <div className="flex items-center gap-6">
+        <img src={logo} alt="Logo" className="h-10 w-10" />
         <span className="text-white text-[1.6rem] font-extrabold font-[Instrument Sans]">
           FoodSpace
         </span>
       </div>
 
-      {/* Center Navigation */}
-      <nav className="flex gap-15">
-        <span className="text-white text-xl font-[Montserrat] cursor-pointer">
+
+      <nav className="flex gap-25">
+        <span className="text-white text-[1.3rem] font-[Montserrat] font-bold cursor-pointer">
           Home
         </span>
-        <span className="text-white text-xl font-[Montserrat] cursor-pointer">
+        <span className="text-white text-[1.3rem]  font-[Montserrat] font-bold cursor-pointer">
           Menu
         </span>
-        <span className="text-white text-xl font-[Montserrat] cursor-pointer">
+        <span className="text-white text-[1.3rem]  font-[Montserrat] font-bold cursor-pointer">
           Community
         </span>
       </nav>
 
-      {/* Right Icons */}
       <div className="flex gap-10">
         <FaUserCircle className="text-white text-2xl cursor-pointer" />
         <FaBell className="text-white text-2xl cursor-pointer" />
--- Frontend/front_work/src/components/Landing.jsx
+++ Frontend/front_work/src/components/Landing.jsx
@@ -1,42 +1,31 @@
 import { motion } from "framer-motion";
-import recipe from '../assets/recipe.png';
-import thali from '../assets/thali.png';
-import masalas from '../assets/masalas.png';
-import cook from '../assets/cook.png';
-import south from '../assets/south.png';
-import healthy from '../assets/healthy.png';
-import food from '../assets/food.png';
-import wood from '../assets/wood.jpeg';
-
-
-
 
 export default function WelcomeSection() {
   return (
-    <div className="p-6 flex flex-col items-center mt-6">
+    <div className="p-4 flex flex-col items-center mt-20 font-[Poppins]">
       <div className="flex flex-row justify-center">
         <motion.p
-          className="overflow-hidden text-[6rem] font-extrabold text-white leading-none whitespace-nowrap"
+          className="overflow-hidden text-[5rem] font-extrabold text-white leading-none whitespace-nowrap"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ duration: 0.7, delay: 1 }}
+          transition={{ duration: 1, ease: "easeInOut", delay: 0.5 }}
         >
           Welcome,
         </motion.p>
 
-        <div className="overflow-hidden flex gap-5 text-[6rem] font-extrabold text-white leading-none whitespace-nowrap">
+        <div className="overflow-hidden flex gap-5 text-[5rem] text-white font-extrabold leading-none whitespace-nowrap">
           <motion.p
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 0.7, delay: 1.2 }}
+            transition={{ duration: 1, ease: "easeInOut", delay: 0.7 }}
           >
             To
           </motion.p>
           <motion.p
-            className="text-amber-600"
-            initial={{ opacity: 0, x: -30 }}
+            className="overflow-hidden bg-gradient-to-r from-orange-500 to-red-500 bg-clip-text text-[5rem] text-transparent"
+            initial={{ opacity: 0, x: "-10%" }}
             animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5, delay: 1.4 }}
+            transition={{ duration: 1, ease: "easeInOut", delay: 0.9 }}
           >
             FoodSpace
           </motion.p>
@@ -44,100 +33,51 @@ export default function WelcomeSection() {
       </div>
 
       <motion.hr
-        className="overflow-hidden bg-amber-500 border-amber-500 border h-1 mt-4"
+        className="overflow-hidden bg-amber-500 border-amber-500 border h-1 mt-3 w-[60%]"
         initial={{ opacity: 0, width: "0%" }}
-        animate={{ opacity: 100, width: "70%" }}
-        transition={{ duration: 0.8, delay: 1.8, ease: "easeOut" }}
+        animate={{ opacity: 1, width: "60%" }}
+        transition={{ duration: 1, ease: "easeInOut", delay: 1.3 }}
       />
 
-      <div className="flex gap-20 justify-center text-3xl mt-4 mr-5 text-white">
+      <div className="flex gap-10 justify-center text-2xl mt-3 text-gray-300 font-[Lato] text-center">
         <motion.span
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8, delay: 2 }}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.2, ease: "easeInOut", delay: 1.5 }}
         >
           Amazing Recipes
         </motion.span>
 
         <motion.hr
-          className="overflow-hidden bg-amber-500 border-amber-500 border h-10 "
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 100 }}
-          transition={{ duration: 0.8, delay: 2.1, ease: "easeOut" }}
+          className="overflow-hidden bg-amber-500 border-amber-500 border h-8"
+          initial={{ opacity: 0, height: "0px" }}
+          animate={{ opacity: 1, height: "40px" }}
+          transition={{ duration: 1.2, ease: "easeInOut", delay: 1.7 }}
         />
 
         <motion.span
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8, delay: 2.3 }}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeInOut", delay: 1.9 }}
         >
           Healthy Choices
         </motion.span>
 
         <motion.hr
-          className="overflow-hidden bg-amber-500 border-amber-500 border h-10"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 100 }}
-          transition={{ duration: 0.8, delay: 2.5, ease: "easeOut" }}
+          className="overflow-hidden bg-amber-500 border-amber-500 border h-8"
+          initial={{ opacity: 0, height: "0px" }}
+          animate={{ opacity: 1, height: "40px" }}
+          transition={{ duration: 0.8, ease: "easeInOut", delay: 2.1 }}
         />
 
         <motion.span
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8, delay: 2.5 }}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeInOut", delay: 2.3 }}
         >
-          Vivid Expereinces
+          Vivid Experiences
         </motion.span>
-
       </div>
-
-      {/* <div className=" overflow-hidden relative w-full p-0 h-96 mt-5 flex">
-        <img src={recipe} alt="Recipe Book" className="h-[300px] w-auto absolute bottom-50 left-[-5%] z-10" />
-        <img src={thali} alt="Thali" className="h-[250px] w-auto absolute bottom-[-13%] left-[-6%] z-20" />
-        <img src={masalas} alt="Masalas" className="h-[250px] w-auto absolute bottom-[-25%] left-[15%] z-10" />
-        <img src={south} alt="South" className="h-[250px] w-auto absolute bottom-[-25%] left-[32%] z-10" />
-        <img src={healthy} alt="healthy" className="h-[250px] w-auto absolute bottom-[-25%] left-[43%] z-10" />
-        <img src={food} alt="food" className="h-[250px] w-auto absolute bottom-[-14%] left-[65%] z-10" />
-        <img src={cook} alt="Cooking" className="h-[450px] w-auto absolute bottom-[-50px] left-[82%] z-20" />
-        <img src={wood} alt="food" className="h-[300px] w-50 absolute bottom-[-40%] left-[15%] rotate-[-3deg]" />
-      </div> */}
-
-      <div className="flex flex-col mt-10">
-          <p className="text-lg font-bold text-white text-center border-b border-gray-500 pb-2">
-            What is FoodSpace ?
-          </p>
-              <div className="flex flex-row gap-30 mt-10">
-                <div className="relative w-72 p-4 bg-white text-black rounded-md shadow-lg border border-gray-300 border-dashed font-mono before:content-[''] before:absolute before:bg-red-500 before:w-4 before:h-4 before:rounded-full before:top-[-10px] before:left-[50%] before:translate-x-[-50%]">
-                  <h2 className="text-lg font-bold text-gray-800 text-center border-b border-gray-500 pb-2">🍽️ Order Slip</h2>
-                  <p className="mt-3 text-sm">
-                    FoodSpace is a platform where you can search, create, and share recipes of your choice.
-                    It provides a wide range of recipes along AI-powered suggestions to improve your cooking experience.
-                  </p>
-                  <div className="border-t border-gray-300 mt-2 pt-2 text-center text-xs text-gray-500">
-                    Space for food images 
-                  </div>
-                </div>
-
-              
-                <div className="relative w-72 p-4 bg-white text-black rounded-md shadow-lg border border-gray-300 border-dashed font-mono before:content-[''] before:absolute before:bg-red-500 before:w-4 before:h-4 before:rounded-full before:top-[-10px] before:left-[50%] before:translate-x-[-50%]">
-                  <h2 className="text-lg font-bold text-gray-800 text-center border-b border-gray-500 pb-2">🍽️ Order Slip</h2>
-                  <p className="mt-3 text-sm">
-                    FoodSpace acts as a community platform for aspiring chefs to share their recipes and get feedback. 
-                    Start exploring now and to unleash your inner chef!
-                  </p>
-                  <div className="border-t border-gray-300 mt-2 pt-2 text-center text-xs text-gray-500">
-                    Space for food images 
-                  </div>
-                 </div>
-                 </div>
-      </div>
-
-
-
-
-
-
-
     </div>
   );
 }
